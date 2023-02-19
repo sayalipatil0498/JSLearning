eb0a8af6436a283843b5f565ede79bd411003703
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-02-19 23:30:01 +0530

    02_datatypes.js
    02_datatypes.js

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,18 @@
+var name="Nikhil";
+var bDate=03;
+var marriage=false;
+
+var typeofName= typeof name;
+console.log("name:", name ,          "type of name variable is:",typeof name);
+console.log("birthdate:",bDate,        "type of bDate variable is:", bDate);
+console.log("marriage:",marriage ,      "type of marriage variable is:",typeof marriage);
+
+
+
+
+console.log("********Diff between null & undefind********")
+var age=null;
+console.log("type of age variable is:",age);
+
+var sirName;
+console.log("type of sirName is:",sirName);
---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+var myname = "Vamika Kohli";
+var fatherName = "Virat Kohli";
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false;
+
+var typeofMyName = typeof myname;
+console.log("typeof variable myName:", typeofMyName);
+console.log("typeof variable fatherName:",typeof fatherName);
+console.log("typeof variable age:",typeof age);
+console.log("typeof variable weight:",typeof weight);
+console.log("type of variable isMarried",typeof isMarried);
+console.log("type of variable isLazy:",typeof isLazy);
+
+var graduation; //graduation variable which can store undefined type of value
+console.log("value of graduation variable is:",graduation);
+console.log("type of graduation is:",typeof graduation);
+
+var city=null;//i want to make a variable as a empty value
+console.log("value of city variable is:",city);
+console.log("type of city variable is:",typeof city);
+
+age="32"; //anything inclosed in single quote or double quote this is called string
+console.log("typeof age"); 
