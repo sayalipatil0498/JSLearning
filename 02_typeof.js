695b75ed03fa1cba1cb06eb695c0ba951817f706
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-02-20 17:14:20 +0530

    02_typeof.js
    02_typeof.js

---
+++ 02_typeof.js
@@ -0,0 +1,20 @@
+var bankName;
+console.log("type of bankName is:",typeof bankName);
+bankName="SBI Bank";
+console.log("type of bankName datatype is:", typeof bankName);
+console.log("value of bankName: " , bankName);
+
+var marks = '90%';
+console.log("value of marks variable:",marks);
+console.log( "type of variable marks:",typeof marks);
+
+var isWorking;
+console.log("type of isWorking is:",typeof isWorking);
+isWorking = true;
+console.log("value of isWorking is:",isWorking);
+console.log("type of datatype:",typeof isWorking);
+
+var totalCount="Hundred and Seven";
+console.log("value of totalCount:",totalCount, "type of totalCount:",typeof totalCount);
+totalCount=107;
+console.log("value of TotalCount:",totalCount, "type of totalCount:",typeof totalCount);
