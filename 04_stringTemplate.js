599100c82cf2be04b12954271475b9bfd4b1b02a
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-02-22 21:35:17 +0530

    04_stringTemplate.js

---
+++ 04_stringTemplate.js
@@ -0,0 +1,7 @@
+console.log(`You are "champ" man......`);
+
+var firstName = "sayali";
+var lastName = "patil";
+var city = "karad";
+
+console.log(`my first name ${firstName} last name ${lastName} city ${city}`); //variable substitution
