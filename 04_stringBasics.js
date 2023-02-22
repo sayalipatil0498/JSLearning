eb36671714d1d4fc6fea45d3993ebf6949d631ab
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-02-22 21:07:16 +0530

    04_stringBasics.js

---
+++ 04_stringBasics.js
@@ -0,0 +1,29 @@
+function stringBasics(){
+    console.log("My dream company name is: TCS");
+   
+}
+stringBasics();
+console.log("-------------second--------------");
+
+var hobby1, hobby2, hobby3;
+    hobby1= "Coding";
+    hobby2= "Reading";
+    hobby3= "Writing";
+    console.log("My hobbies includes: ",hobby1, hobby2, hobby3);
+    console.log("concated String as below:\n"+hobby1+hobby2+hobby3);
+    console.log("total sum of hobby1 char: ", hobby1.length);
+   // var hobby1length = hobby1.length;
+    //console.log(hobby1length);
+    console.log("total sum of hobby2 char: ", hobby2.length);
+    //var hobby2length = hobby2.length;
+    //console.log(hobby2length);
+    console.log("total sum of hobby3 char: ", hobby3.length);
+    //var hobby3length = hobby3.length;
+    //console.log(hobby3length);
+
+
+
+    
+
+
+
