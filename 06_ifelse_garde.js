055918ed655d73b8f2734e09d26d10d27f0d9581
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-02-28 23:13:13 +0530

    06_ifelse_grade.js
    06_ifelse_grade.js

--- 06_ifelse_garde.js
+++ 06_ifelse_garde.js
@@ -1,40 +1,72 @@
 function voteEligibility(age){ 
-    if(age<=0 || age>120 || age==undefined || age==null)
+    if(null ||age<=0 || age>120 ||(typeof age!="number"))
     {
-       return `Your age  : ${age} this is an invald data`;
-    }
+        console.log(`Your age  : '${age}' this is an invalid data`);
+    }else{
     if(age>=18){
-        var str = `Hey your age is : ${age} so you are eligible for voting`;
-        return str;
+        console.log( `Hey your age is : '${age}' so you are eligible for voting`);
         
     }
     else
     {
-        var str =`Hey your age is : ${age} so you are not eligible for voting`;
-        return str;
+        console.log(`Hey your age is : '${age}' so you are not eligible for voting`);
+    
         }
+    }
 
 }
-var result = voteEligibility(45);
-console.log(result);
-var result = voteEligibility(17);
-console.log(result);
-var result = voteEligibility(8);
-console.log(result);
-var result = voteEligibility(20)
-console.log(result);
-var result =voteEligibility(-10);
-console.log(result);
-var result = voteEligibility(200);
-console.log(result);
-var result =voteEligibility(0);
-console.log(result);
-var result = voteEligibility(undefined);
-console.log(result);
-var result = voteEligibility(null);
-console.log(result);
+voteEligibility(45);
+voteEligibility(17);
+voteEligibility(8);
+voteEligibility(20)
+voteEligibility(-10);
+voteEligibility(200);
+voteEligibility(0);
+voteEligibility(undefined);
+voteEligibility(null);
+
 
 console.log("-----------step2-------------");
 function gradeCalculation(marks){
+    if(marks==0 || marks<0 ||marks>100 || (typeof marks!="number")){
+        console.log(`Invalid marks : ${marks} , Please provide the valid Marks`);
+    }
+    else{
+    if(marks>=90){
+        console.log(`Funtastic marks: ${marks}, your grade is A+`);
+    }else{
+        if(marks>=75 && marks<=90){
+        console.log(`Excellent marks: ${marks}, your grade is A`);
+        }
+        else{
+            if(marks>=50 && marks<=75){
+                console.log(`Good marks: ${marks}, your grade is B`);
+                }
+                else{
+                    if(marks>=35 && marks<=50){
+                        console.log(` Marks is : ${marks}, your grade is C,Need improvement`);
+                        }
+        
+
+                        }
+
+                }
+            
+        }
+    }
     
 }
+gradeCalculation(98);
+gradeCalculation(80);
+gradeCalculation(90);
+gradeCalculation(0);
+gradeCalculation(150);
+gradeCalculation(-7);
+gradeCalculation(35);
+gradeCalculation(29);
+gradeCalculation(64);
+gradeCalculation(49);
+gradeCalculation("91");
+gradeCalculation("Eighty");
+gradeCalculation(NaN);
+gradeCalculation(null);
