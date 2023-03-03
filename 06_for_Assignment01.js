96820b64b38f4e2be657f3ea706a78b941edbe8e
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-03-03 17:05:36 +0530

    06_for_Assignment01.js
    06_for_Assignment01.js

--- 06_for_Assignment01.js
+++ 06_for_Assignment01.js
@@ -1,19 +1,38 @@
-//console("---------step----------");
-for (let index = 5; index <= 50; index++) {
+console.log("Wap to print numbers from 5 to 15 by incrementing 1");
+for (let index = 5; index <= 15; index++) {
     console.log(index);
     
 }
-
-//console.log("--------step2--------");
+console.log("Wap to print numbets from 50 to 40 by decrementing 1");
 for (let index = 50; index >=40; index--) {
     console.log(index);
     
 }
-
-for (let index = 0; index<=15%2==1 ; index++) {
-
-
+console.log("Wap to print first 15 odd numbers ");
+for (let index = 1; index <= 15 ; index++) {
+    if(index % 2 == 1){
+     console.log(index);
+    } 
     
+}
+console.log("Wap to print first 10 even numbers");
+for (let index = 0; index <= 10; index++) {
+    if(index %2 == 0){
+    console.log(index);
+    }
+    
+}
+console.log("Wap to print table of 5");
+for (let index = 5; index <=50; index = index+5) {
     console.log(index);
     
+}
+console.log("Wap to print table of 10");
+for (let index = 10; index <=100; index = index+10) {
+    console.log(index);
+    
+}
+console.log("Wap to print table of 10 in reverse order");
+for (let index = 100; index >=10; index = index-10) {
+    console.log(index);
 }
