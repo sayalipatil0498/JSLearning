4e11cf72739ce0e6074ae26dfb208cc3f5a54aa6
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-03-05 21:13:29 +0530

    06_factorial_B.js
    06_factorial_B.js

--- 06_factorial_B.js
+++ 06_factorial_B.js
@@ -1,15 +1,30 @@
-function factorialWordsCount(word){
-    var fact = 1;
-    for (let index = word; index >=1; index--) {
-        fact = fact * index;
-
-        
-        
+function factorialWordsCount(str){
+    if(str==null || str== undefined || (isNaN(str) && typeof str=="number")){
+       console.log(`Given string ${str} is invalid`);
+    }else{
+        console.log(`Given string is valid :${str}`);
+        let words = str.split(" ");
+        const totalWords = words.length;
+        console.log(totalWords);
+        let factorial = 1;
+        for (let index = totalWords; index >=1; index--) {
+          factorial = index * factorial;
+          //console.log(`Factorial of "${str}" is ${factorial}`);
+            
+        }
+       return `Factorial of "${str}" is ${factorial}`;
     }
-
 }
-factorialWordsCount("Revision is the mother of sucess");
-factorialWordsCount("We never fail,we always learn");
-factorialWordsCount(null);
-factorialWordsCount("");
-factorialWordsCount("Sayali Balaso Patil");
+    //console.log(factorialWordsCount("Revision is the mother of sucess"));
+
+
+var result = factorialWordsCount("Revision is the mother of sucess");
+console.log(result);
+ var result = factorialWordsCount("We never fail,we always learn");
+ console.log(result);
+var result =factorialWordsCount(null);
+console.log(result);
+var result =factorialWordsCount("");
+console.log(result);
+var result =factorialWordsCount("Sayali Balaso Patil");
+console.log(result);
