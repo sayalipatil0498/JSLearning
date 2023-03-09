25410cc50db84c3098e9b476a1888eb818696688
sayalipatil0498 <sayalipatil0498@gmail.com>
2023-03-10 00:55:14 +0530

    09_object_assignE.js
    09_object_assignE..js

---
+++ 09_object_assignE.js
@@ -0,0 +1,50 @@
+const sbiBank = {
+    bankName :"State bank of India",
+    location : "Karad",
+    accountNo : "130002332322",
+    ifscCode : "SBI0033",
+    interestRate : "10.25",
+     showDetails :function(){
+        console.log(`Bank Name :${sbiBank.bankName},Location :${sbiBank.location},AccountNo :${sbiBank.accountNo},IFSCCode : ${sbiBank.ifscCode},InterestRate :${sbiBank.interestRate}`);
+    }
+}
+sbiBank.showDetails();
+
+
+const axisBank = {
+    bankName :"Axis Bank",
+    location : "Pune",
+    accountNo : "130002332322",
+    ifscCode : "AB0033",
+    interestRate : "10.25",
+     showDetails :function(){
+        console.log(`Bank Name :${axisBank.bankName},Location :${axisBank.location},AccountNo :${axisBank.accountNo},IFSCCode : ${axisBank.ifscCode},InterestRate :${axisBank.interestRate}`);
+    }
+}
+axisBank.showDetails();
+
+
+const hdfcBank = {
+    bankName :"HDFC Bank",
+    location : "Mumbai",
+    accountNo : "130002332322",
+    ifscCode : "HDFC0033",
+    interestRate : "10.25",
+     showDetails :function(){
+        console.log(`Bank Name :${hdfcBank.bankName},Location :${hdfcBank.location},AccountNo :${hdfcBank.accountNo},IFSCCode : ${hdfcBank.ifscCode},InterestRate :${hdfcBank.interestRate}`);
+    }
+}
+hdfcBank.showDetails();
+
+
+const yesBank = {
+    bankName :"Yes Bank",
+    location : "Kolhapur",
+    accountNo : "130002332322",
+    ifscCode : "YES0033",
+    interestRate : "10.25",
+     showDetails :function(){
+        console.log(`Bank Name :${yesBank.bankName},Location :${yesBank.location},AccountNo :${yesBank.accountNo},IFSCCode : ${yesBank.ifscCode},InterestRate :${yesBank.interestRate}`);
+    }
+}
+yesBank.showDetails();
